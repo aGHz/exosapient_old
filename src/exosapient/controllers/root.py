316355e6ce9838dc3c42c4cc51f03828d28ac18f
Adrian Ghizaru <adrian.ghizaru@gmail.com>
2012-10-06 12:56:39 -0400

    Testing global persistence across requests

--- src/exosapient/controllers/root.py
+++ src/exosapient/controllers/root.py
@@ -4,9 +4,16 @@ from exosapient.util.response import *
 
 log = __import__('logging').getLogger(__name__)
 
+FOOBAR = 0
 
 class RootController(Controller):
+    def test(self, *args, **kwargs):
+        global FOOBAR
+        FOOBAR = 108
+        return TEMPLATE('index', {'project_name': FOOBAR})
+
     def __default__(self, *args, **kwargs):
+        global FOOBAR
         log.info('-- Request --')
-        return TEMPLATE('index', {'project_name': 'exosapient'})
+        return TEMPLATE('index', {'project_name': FOOBAR})
 
