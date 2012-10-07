b610b244a74fb8ee7dfd737075d85dd1a792959d
Adrian Ghizaru <adrian.ghizaru@gmail.com>
2012-10-07 15:05:19 -0400

    Cleaned out deprecated methods in scraping.Session

--- src/exosapient/util/scraping.py
+++ src/exosapient/util/scraping.py
@@ -10,7 +10,6 @@ UA_STRINGS = {
     }
 
 
-
 class Session(object):
     def __init__(self, jar=None, ua_string='chrome21'):
         # Prepare cookie jar
@@ -40,7 +39,6 @@ class Session(object):
         resp = self.opener.open(url, urllib.urlencode(data))
         return (resp, resp.read())
 
-
     def cookies(self, name=None):
         cookies = [c for c in self.jar]
         if name is not None:
@@ -53,67 +51,6 @@ class Session(object):
         print '--- [cookie jar] ' + '-' * 62
         for cookie in self.jar: print cookie
         print '-' * 79
-
-    # --- deprecated ---
-
-    def submit_form(self, action, data, clear_next=True):
-        self.prev_url = action
-        self.prev_data = data
-
-        resp = self.opener.open(action, urllib.urlencode(data))
-        if clear_next: self.clear_next()
-        return (resp, resp.read())
-
-    def get_page(self, url, clear_next=True):
-        self.prev_url = url
-        self.prev_data = None
-
-        resp = self.opener.open(url)
-        if clear_next: self.clear_next()
-        return (resp, resp.read())
-
-    def parse_form(self, form):
-        if form.name != 'form':
-            raise Exception('Session.parse_form did not receive a BeautifulSoup form')
-
-        inputs = {}
-        for inp in form.find_all('input'):
-            inputs[inp['name']] = dict(inp.attrs)
-
-        data = dict(zip(inputs.keys(), [attrs.get('value', '').encode('ascii', 'ignore') for attrs in inputs.values()]))
-        self.next_data = data
-
-        if self.prev_url is not None:
-            self.next_url = urlparse.urljoin(self.prev_url, form['action'])
-        else:
-            # TODO what if it's not a full URL?
-            self.next_url = form['action']
-
-        return (form['action'], form['method'], inputs, data)
-
-    def submit_next(self, data={}):
-        if self.next_url is None:
-            raise Exception('Session.submit called with nowhere to go')
-        if self.next_data is None:
-            self.next_data = {}
-
-        self.next_data.update(data)
-        return self.submit_form(self.next_url, self.next_data)
-
-    def data(self):
-        return self.next_data
-    def set_data(self, k, v):
-        self.next_data[k] = v
-
-    def clear_prev(self):
-        self.prev_url = None
-        self.prev_data = None
-    def clear_next(self):
-        self.next_url = None
-        self.next_data = None
-    def clear_all(self):
-        self.clear_prev()
-        self.clear_next()
 
 
 class Scraper(object):
