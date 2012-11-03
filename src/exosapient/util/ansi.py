9706b2af780560814c1fd0bb61c665faee6a41be
Adrian Ghizaru <adrian.ghizaru@gmail.com>
2012-11-03 13:46:51 -0400

    Added exported dictionary of ansi colors for easy formatting

--- src/exosapient/util/ansi.py
+++ src/exosapient/util/ansi.py
@@ -3,3 +3,16 @@ bold = "\033[1m"
 reset = "\033[0m"
 
 black, red, green, yellow, blue, magenta, cyan, white = [_color % i for i in range(30, 38)]
+
+colors = {
+    'black': black,
+    'red': red,
+    'green': green,
+    'yellow': yellow,
+    'blue': blue,
+    'magenta': magenta,
+    'cyan': cyan,
+    'white': white,
+    'bold': bold,
+    'reset': reset,
+    }
